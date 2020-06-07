8f69d98c13ae0d286ea63a26f30b67da07ad15d0
CHINOBv <retrochanel97@gmail.com>
2020-06-07 06:54:54 -0700

    Agregue la funcion para eliminar usuarios, crear nuevos usuarios y para obtener los usuarios la hice real-time con la funcion de getUsers, me falta editar usuarios

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -1,33 +1,126 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const API = process.env.REACT_APP_API;
 
 const Users = () => {
-  const [Name, setName] = useState("");
-  const [Email, setEmail] = useState("");
-  const [Password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [Password_Rep, setPassword_Rep] = useState("");
+
+  const [users, setUsers] = useState([]);
+  const [Alert, setAlert] = useState({
+    status: false,
+    msg: "",
+    color: "",
+  });
+
   const [Error, setError] = useState({
-    state: false,
+    status: false,
+    type: "",
     msg: "",
   });
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (Password.trim() !== Password_Rep.trim()) {
-      setError({ state: true, msg: "Both Passwords Must Match" });
+
+  useEffect(() => {
+    if (password.trim() !== Password_Rep.trim()) {
+      setError({
+        status: true,
+        type: "fields",
+        msg: "Both Passwords Must Match",
+      });
       return;
-    }else{
-      setError({})
+    } else {
+      setError({});
     }
+    if (
+      password.trim() === "" ||
+      Password_Rep.trim() === "" ||
+      name.trim() === "" ||
+      email.trim() === " "
+    ) {
+      setError({
+        status: true,
+        type: "fields",
+        msg: "All Fields Are Required",
+      });
+      return;
+    }
+    setError({});
+  }, [name, email, password, Password_Rep]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (Error.status) return;
+
+    let res = await axios(`${API}/users`, {
+      method: "POST",
+      data: {
+        name,
+        email,
+        password,
+      },
+    });
+    // console.log(res);
+    await getUsers();
+    if (!res) {
+      setAlert({
+        status: true,
+        color: "danger",
+        msg: "An error occurred while creating the user",
+      });
+      return;
+    }
+    setAlert({
+      status: true,
+      color: "success",
+      msg: "User Created",
+    });
+    setTimeout(() => {
+      setAlert({});
+    }, 5000);
+  };
+
+  const getUsers = async () => {
+    let res = await axios(`${API}/users`);
+    let data = await JSON.stringify(res.data);
+    let datas = await JSON.parse(data);
+    await setUsers(datas);
+    //console.log(datas)
+  };
+
+  useEffect(() => {
+    setError({});
+
+    getUsers();
+  }, []);
+
+  const deleteUser = async (id) => {
+    if (!window.confirm("Do You Want to Delete this User?")) return;
+    await axios(`${API}/users/${id}`, {
+      method: "DELETE",
+    });
+    await setAlert({
+      status: true,
+      msg: "User Deleted",
+      color: "danger",
+    });
+    await getUsers();
+    setTimeout(() => {
+      setAlert({});
+    }, 5000);
   };
 
   return (
     <div className="row">
       <div className="col-md-4">
-        {Error.state ? (
+        {Error.status && Error.type === "fields" ? (
           <div
             className="alert alert-warning alert-dismissible fade show"
             role="alert"
           >
-           {Error.msg}
+            {Error.msg}
             <button
               type="button"
               className="close"
@@ -44,7 +137,7 @@ const Users = () => {
               type="text"
               className="form-control"
               onChange={(e) => setName(e.target.value)}
-              value={Name}
+              value={name}
               placeholder="Enter your Name"
               autoFocus
             />
@@ -54,7 +147,7 @@ const Users = () => {
               type="email"
               className="form-control"
               onChange={(e) => setEmail(e.target.value)}
-              value={Email}
+              value={email}
               placeholder="Enter your Email"
               autoFocus
             />
@@ -64,7 +157,7 @@ const Users = () => {
               type="password"
               className="form-control"
               onChange={(e) => setPassword(e.target.value)}
-              value={Password}
+              value={password}
               placeholder="Enter your Password"
               autoFocus
             />
@@ -80,13 +173,61 @@ const Users = () => {
             />
           </div>
           <input
+            disabled={Error.type === "fields" ? true : false}
             type="submit"
             value="Create"
             className="btn btn-primary btn-block"
           />
         </form>
       </div>
-      <div className="col-md-8"></div>
+      <div className="col-md-6">
+        {Alert.status ? (
+          <div
+            className={`alert alert-${Alert.color} alert-dismissible fade show alert`}
+            role="alert"
+          >
+            {Alert.msg}
+            <button
+              type="button"
+              className="close"
+              data-dismiss="alert"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">×</span>
+            </button>
+          </div>
+        ) : null}
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Password</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id}>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.password}</td>
+                <td>
+                  <button className="btn btn-ligth btn-sm btn-block border border-primary">
+                    Edit
+                  </button>
+                  <button
+                    className="btn btn-danger btn-sm btn-block"
+                    onClick={() => deleteUser(user._id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
